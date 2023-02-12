21ac8af7f578277fa7bf3b9e6bbc6686b0772940
ira-vladyko <i.vladik2299@gmail.com>
2023-02-12 19:07:21 +0100

    add function get followers

---
+++ config.example.js
@@ -0,0 +1,11 @@
+const username = 'username';
+const password = 'usernamepassword'
+const defaultTimeout = 2000
+
+
+module.exports = {
+    username,
+    password,
+    defaultTimeout
+
+}
--- index.js
+++ index.js
@@ -1,112 +1,52 @@
 const { IgApiClient } = require('instagram-private-api')
+const { username, password, defaultTimeout } = require('./config')
 
 const ig = new IgApiClient();
 
 function login() {
-    ig.state.generateDevice('');
-    return ig.account.login('', '');
+    ig.state.generateDevice(username);
+    return ig.account.login(username, password);
+}
+
+function sleep(ms = defaultTimeout) {
+    return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+async function getFollowersById(userId) {
+    const followersArr = []
+    const feed = await ig.feed.accountFollowers(userId);
+    do {
+        const followers = await feed.items()
+        const usernameArr = followers.map((follower) => follower.username)
+        followersArr.push(...usernameArr)
+        await sleep()
+
+    } while (feed.isMoreAvailable())
+    return followersArr
+}
+
+async function getFollowersByUsername(username) {
+    const userId = await ig.user.getIdByUsername(username)
+    if (!userId) {
+        return []
+    }
+    const followers = await getFollowersById(userId)
+    return followers
 }
 
 async function main() {
     try {
-        const result = await login()
-        //console.log(result)
-        const myId = await ig.user.getIdByUsername('')
-        console.log(myId)
+        await login()
 
+        const totalFollowers = []
 
-        // const getFolowers = await ig.
-        // console.log(getFolowers)
+        const accounts = ['ira_vladuko', 'gunners__27', 'Timosha_22']
 
-        ///н404 Not Found
-        //const iraInfo = await ig.user.accountDetails(iraId)
-        //console.log(iraInfo)
-
-        //выдает undefined
-        // const info = await ig.user.arlinkDownloadInfo()
-        // console.log(info)
-
-        //404 Not Found
-        // const info = await ig.user.flagUser(iraId)
-        // console.log(info)
-
-        //404 Not Found
-        // const info = await ig.user.formerUsernames(iraId)
-        // console.log(info)
-
-        //404 Not Found
-        // const info = await ig.user.info(iraId)
-        // console.log(info)
-
-
-        //?!
-        // const info = await ig.user.lookup('merhallon')
-        // console.log(info)
-
-        //works
-        // const info = await ig.user.search('merhallon')
-        // console.log(info)
-
-        //works
-        // const info = await ig.user.searchExact('merhallon')
-        // console.log(info)
-
-        //404 not Found
-        // const info = await ig.user.sharedFollowerAccounts('ira_vladuko')
-        // console.log(info)
-
-        //works
-        // const info = await ig.user.usernameinfo('ira_vladuko')
-        // console.log(info)
-
-        // const followers = await ig.friendship.getFollowers(iraId);
-        // console.log(followers);
-
-
-        // const followersArr = []
-        // const feed = await ig.feed.accountFollowers(myId)
-
-        // const followers = await feed.items()
-        // followersArr.push(...followers.map(f => f.username))
-        // await new Promise((resolve) => {
-        //     setTimeout(() => resolve('test'), 3000)
-        // })
-        // const followers2 = await feed.items()
-        // followersArr.push(...followers2.map(f => f.username))
-        // await new Promise((resolve) => {
-        //     setTimeout(() => resolve('test'), 3000)
-        // })
-        // const followers3 = await feed.items()
-        // followersArr.push(...followers3.map(f => f.username))
-        // await new Promise((resolve) => {
-        //     setTimeout(() => resolve('test'), 3000)
-        // })
-
-        // console.log(followersArr)
-        // console.log((new Set(followersArr)).size)
-
-        // const feed2 = await ig.feed.accountFollowers({ id: myId, query: 'next_max_id=130' })
-        // const response2 = await feed2.request()
-        // const followersCount2 = response2.users.length
-        // console.log(followersCount2)
-
-
-        // const followersUsersNames = followers.map((follower) => follower.username)
-        //const followers = await feed.request()
-        // console.log(followersUsersNames, followersUsersNames.length)
-
-
-
-
-
-
-
-
-
-
-
-
-
+        for await (account of accounts) {
+            const followers = await getFollowersByUsername(account)
+            totalFollowers.push(...followers)
+        }
+        console.log(totalFollowers)
 
     }
     catch (error) {
